7100626205a1636eb3a3eb64fe5d3401a9823a1f
Gregory C. Oakes <gregcoakes@gmail.com>
2021-05-16 22:33:29 -0500

    Add movement system.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,33 @@
-use bevy::{input::mouse::MouseMotion, prelude::*};
+use std::f32::consts::PI;
 
-#[repr(transparent)]
-struct Velocity(Vec3);
+use bevy::prelude::*;
+use bevy_advanced_input::{
+    input_id::InputID,
+    plugin::InputBindingPlugin,
+    user_input::{InputAxisType, MouseAxisType, UserInputHandle, UserInputSet},
+};
 
-/// Yaw and pitch rotation in terms of radians.
-#[repr(transparent)]
-struct YPRotation(Vec2);
+struct FPSCameraMovement {
+    pitch: f32,
+    yaw: f32,
+    velocity: Vec3,
+}
 
 struct Player;
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+enum InputType {
+    Game,
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+enum Bindings {
+    Longitudinal,
+    Latitudinal,
+    Elevation,
+    Pitch,
+    Yaw,
+}
 
 pub fn run() {
     let canvas = web_sys::window()
@@ -36,10 +56,12 @@ pub fn run() {
         .insert_resource(Msaa { samples: 4 })
         .insert_resource(win_desc)
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup.system())
-        .add_system(input.system())
-        .add_system(movement.system())
-        .add_system(mouse_look.system());
+        .add_plugin(InputBindingPlugin::<InputType, Bindings>::default())
+        .add_startup_system(setup_input.system())
+        .add_startup_system(spawn_player.system())
+        .add_startup_system(spawn_scene.system())
+        .add_system(player_control.system())
+        .add_system(movement.system());
 
     #[cfg(target_arch = "wasm32")]
     {
@@ -49,12 +71,74 @@ pub fn run() {
     builder.run();
 }
 
-fn setup(
+fn setup_input(mut input_bindings: ResMut<UserInputHandle<InputType, Bindings>>) {
+    let mut input_set = UserInputSet::new();
+
+    input_set
+        .begin_axis(Bindings::Longitudinal)
+        .add(
+            InputAxisType::GamepadAxis(GamepadAxisType::LeftStickY),
+            None,
+        )
+        .add(InputAxisType::KeyboardButton(KeyCode::W), Some(1.0))
+        .add(InputAxisType::KeyboardButton(KeyCode::S), Some(-1.0));
+
+    input_set
+        .begin_axis(Bindings::Latitudinal)
+        .add(
+            InputAxisType::GamepadAxis(GamepadAxisType::LeftStickX),
+            None,
+        )
+        .add(InputAxisType::KeyboardButton(KeyCode::D), Some(1.0))
+        .add(InputAxisType::KeyboardButton(KeyCode::A), Some(-1.0));
+
+    input_set
+        .begin_axis(Bindings::Pitch)
+        .add(InputAxisType::MouseAxisDiff(MouseAxisType::Y), None)
+        .add(
+            InputAxisType::GamepadAxis(GamepadAxisType::RightStickY),
+            None,
+        );
+
+    input_set
+        .begin_axis(Bindings::Yaw)
+        .add(InputAxisType::MouseAxisDiff(MouseAxisType::X), None)
+        .add(
+            InputAxisType::GamepadAxis(GamepadAxisType::RightStickX),
+            None,
+        );
+
+    input_bindings.add_input(InputType::Game, input_set);
+}
+
+fn spawn_player(
+    mut cmds: Commands,
+    mut input_bindings: ResMut<UserInputHandle<InputType, Bindings>>,
+) {
+    cmds.spawn_bundle(PointLightBundle {
+        transform: Transform::from_translation(Vec3::new(0.0, 5.0, -5.0)),
+        ..Default::default()
+    });
+    cmds.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_translation(Vec3::new(0.0, 0.0, -8.0))
+            .looking_at(Vec3::Y, Vec3::Z),
+        ..Default::default()
+    })
+    .insert(FPSCameraMovement {
+        pitch: 0.0,
+        yaw: 0.0,
+        velocity: Vec3::ZERO,
+    })
+    .insert(Player)
+    .insert(input_bindings.create_input_id(InputType::Game));
+}
+
+fn spawn_scene(
     mut cmds: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    for y in -2..=2 {
+    for y in -2..=1 {
         for x in -5..=5 {
             let x01 = (x + 5) as f32 / 10.0;
             let y01 = (y + 2) as f32 / 4.0;
@@ -76,51 +160,46 @@ fn setup(
             });
         }
     }
-    cmds.spawn_bundle(PointLightBundle {
-        transform: Transform::from_translation(Vec3::new(0.0, 5.0, -5.0)),
-        ..Default::default()
-    });
-    cmds.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_translation(Vec3::new(0.0, 0.0, -8.0))
-            .looking_at(Vec3::Y, Vec3::Z),
-        ..Default::default()
-    })
-    .insert(Velocity(Vec3::ZERO))
-    .insert(YPRotation(Vec2::ZERO))
-    .insert(Player);
 }
 
-fn input(keys: Res<Input<KeyCode>>, mut velocities: Query<&mut Velocity, With<Player>>) {
-    for mut velocity in velocities.iter_mut() {
-        velocity.0.z =
-            (keys.pressed(KeyCode::S) as i32 + -(keys.pressed(KeyCode::W) as i32)) as f32;
-        velocity.0.x =
-            (keys.pressed(KeyCode::D) as i32 + -(keys.pressed(KeyCode::A) as i32)) as f32;
-    }
-}
+const MAX_PITCH: f32 = PI / 2.0;
+const MIN_PITCH: f32 = -PI / 2.0;
 
-fn mouse_look(
-    mut mouse_events: EventReader<MouseMotion>,
-    mut transforms_rotations: Query<(&mut Transform, &mut YPRotation), With<Player>>,
+fn player_control(
+    input_bindings: Res<UserInputHandle<InputType, Bindings>>,
+    mut controlled: Query<(&mut FPSCameraMovement, &Transform, &InputID)>,
 ) {
-    let delta: Vec2 = mouse_events
-        .iter()
-        .map(|ev| ev.delta)
-        .fold(Vec2::ZERO, |acc, delta| acc + delta);
+    for (mut movement, transform, input_id) in controlled.iter_mut() {
+        if let Some(input_handle) = input_bindings.to_handle(input_id) {
+            let walk_unit = {
+                let walk = (transform.rotation * -Vec3::Z).normalize_or_zero();
+                Vec3::new(walk.x, 0.0, walk.z).normalize_or_zero()
+            };
+            let right_unit =
+                (Quat::from_rotation_y(90.0f32.to_radians()) * walk_unit).normalize_or_zero();
+            let walkness = input_handle
+                .get_axis_value(Bindings::Longitudinal)
+                .unwrap_or(0.0);
+            let rightness = input_handle
+                .get_axis_value(Bindings::Latitudinal)
+                .unwrap_or(0.0);
+            movement.velocity = (walk_unit * walkness + right_unit * rightness).normalize_or_zero();
 
-    for (mut transform, mut ypr) in transforms_rotations.iter_mut() {
-        ypr.0 += delta / 1000.0;
-        let rotation = Quat::from_rotation_ypr(ypr.0.x, ypr.0.y, 0.0);
-        transform.look_at(Vec3::Y, Vec3::Z);
-        transform.rotate(rotation);
+            let delta = 0.001
+                * input_handle
+                    .get_axis_value(Bindings::Yaw)
+                    .zip(input_handle.get_axis_value(Bindings::Pitch))
+                    .map(|(y, p)| Vec3::new(y, p, 0.0))
+                    .unwrap_or(Vec3::ZERO);
+            movement.yaw += delta.x;
+            movement.pitch = (delta.y + movement.pitch).clamp(MIN_PITCH, MAX_PITCH);
+        }
     }
 }
 
-fn movement(time: Res<Time>, mut players: Query<(&mut Transform, &Velocity)>) {
-    for (mut transform, velocity) in players.iter_mut() {
-        if velocity.0 != Vec3::ZERO {
-            let delta = transform.rotation * velocity.0;
-            transform.translation += delta * time.delta_seconds();
-        }
+fn movement(time: Res<Time>, mut players: Query<(&mut Transform, &FPSCameraMovement)>) {
+    for (mut transform, movement) in players.iter_mut() {
+        transform.translation += movement.velocity * time.delta_seconds();
+        transform.rotation = Quat::from_rotation_ypr(movement.yaw, movement.pitch, 0.0);
     }
 }
